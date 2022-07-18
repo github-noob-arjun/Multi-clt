5c5b632678bd440288785683266057a254ae35ad
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 23:18:57 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,26 +1,9 @@
 from pyrogram import Client, idle, filters
 import os
 import asyncio
-from start import App1, App2, App3
-
-#my_apps = {
-#    "App1": Client("App1"),
- #   "App2": Client("App2"),
-#    "App3": Client("App3"),
-#    # and so on
-#}
-
-
-#for _, app in my_apps.items():
-#    # Add a MessageHandler to each Client and start it
-    # app.add_handler(MessageHandler(test, Filters.command("test")))
-#    app.start()
-
-
-#Client.idle()
-
-#for _, app in my_apps.items():
-#    app.stop()
+from new1.samle1 import App1
+from new2.samle2 import App2
+from new3.samle3 import App3
 
 App1.start()
 App2.start()
