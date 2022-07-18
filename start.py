34f138a261c373c94b5b802829173a5834ce0b4f
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 14:23:08 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,17 +1,7 @@
 from pyrogram import Client, idle, filters 
 import os
 from new.mkn import MKN
-
-BOT_TOKEN = "5465507667:AAFggC4o13JV-J-zC8yuZf3sUZa-1y5AJgk"
-API_ID = 4738674
-API_HASH = "f2be74eaa9b1cb32498f45d04e4dbb54"
-
-bot = Client("Sbot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
-
-@bot.on_message(filters.text)
-async def start(client, message):
-    await message.reply_text("Success Message 💥")
-
+from sample2.mkn2 import Sbot
 
 bot.run()
 MKN.run()
