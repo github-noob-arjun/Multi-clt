2d612df448f5ef0ce3b0935625828d19007e202c
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 23:22:47 +0530

    Delete Bot directory

--- Bot/App2.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
-from start import App2 as Clt2
-
-@Clt2.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("Success Message 💥 2")
--- Bot/App3.py
+++
@@ -1,7 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
-from start import App3 as Clt3
-
-@Clt3.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("Success Message 💥 3")
