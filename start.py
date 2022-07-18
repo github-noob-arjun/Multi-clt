5f0aec2168fdf37ab3cd922a73b933ac475a5694
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 18:39:30 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, idle, filters, compose
 import os
+import asyncio
 from new.mkn import MKN
 from sample2.mkn2 import Sbot
 from sample3.arjun import Arjun
@@ -11,11 +12,6 @@ async def main():
         Client("Sbot"),
         Client("MKN")
     ]
-
-    ...
-
-    await compose(apps)
-
 
 asyncio.run(main())
 
