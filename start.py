bd03296b451154813a0cda83cc2ab14c3d3d1b91
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 22:16:59 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,19 +1,45 @@
 from pyrogram import Client, idle, filters
 import os
 import asyncio
-from new.mkn import MKN
-from sample2.mkn2 import Sbot
-from sample3.arjun import Arjun
+
+App1 = Client(
+      "app1",
+      bot_token="5589185991:AAGDjCCcp7IG1u_YQErui5yLwyfXizptXYI",
+      api_id="4738674",
+      api_hash="f2be74eaa9b1cb32498f45d04e4dbb54",
+)
+
+App2 = Client(
+      "app2",
+      bot_token="5465507667:AAFggC4o13JV-J-zC8yuZf3sUZa-1y5AJgk",
+      api_id="4738674",
+      api_hash="f2be74eaa9b1cb32498f45d04e4dbb54",
+)
+
+App3 = Client(
+      "app3",
+      bot_token="5501750004:AAGXvnWJWR5SRAm5geuv1qikpUVsI2oPK4o",
+      api_id="4738674",
+      api_hash="f2be74eaa9b1cb32498f45d04e4dbb54",
+)
 
 
-async def main():
-    apps = [
-        Client("Arjun"),
-        Client("Sbot"),
-        Client("MKN")
-    ]
-
-asyncio.run(main())
+my_apps = [
+    Client("App1"),
+    Client("App2"),
+    Client("App3"),
+    # and so on
+]
 
 
+for app in my_apps:
+    # Add a MessageHandler to each Client and start it
+    # app.add_handler(MessageHandler(test, Filters.command("test")))
+    app.start()
+
+
+Client.idle()
+
+for app in my_apps:
+    app.stop()
 
