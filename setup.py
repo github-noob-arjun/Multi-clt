ad93f87f986da3299178c64c8f30e0a3b8a0df69
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 22:22:05 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Client',
+      version='0.2.1',
+      description='Python module for interacting with Chatbase APIs',
+      url='https://github.com/github-noob-arjun/Multi-clt',
+      author='Github',
+      author_email='None',
+      license='MIT Pro',
+      packages=['Client'],      
+      zip_safe=False)
