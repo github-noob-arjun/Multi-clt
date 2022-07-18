a8624335abd58436e4d763f3bac36815c5bd543e
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 22:40:38 +0530

    Create App1.py

---
+++ Bot/App1.py
@@ -0,0 +1,7 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
+from start import App1 as Clt1
+
+@Clt2.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("Success Message 💥 1")
