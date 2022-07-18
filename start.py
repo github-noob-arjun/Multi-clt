e2e4f33ebb7f50bfc01fae12d7ea3b423e331f6f
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 16:34:53 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,13 +1,28 @@
-from pyrogram import Client, idle, filters 
+from pyrogram import Client, idle, filters, MessageHandler
 import os
 from new.mkn import MKN
 from sample2.mkn2 import Sbot
 from sample3.arjun import Arjun
 
-Sbot.start()
-MKN.start()
 
-idle()
 
-MKN.stop()
-Sbot.stop()
+my_apps = {
+    "Sbot": Client("Sbot"),
+    "MKN": Client("MKN"),
+    "Arjun": Client("Arjun"),
+    # and so on
+}
+
+def test(a, m):
+    m.reply("Response")
+
+for _, app in my_apps.items():
+    # Add a MessageHandler to each Client and start it
+    app.add_handler(MessageHandler(test, Filters.command("test")))
+    app.start()
+
+Client.idle()
+
+
+for _, app in my_apps.items():
+    app.stop()
