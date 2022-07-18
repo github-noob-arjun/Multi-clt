c2020f3a93ecb17b27062f9a1ef2484a6e5dc844
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 10:11:35 +0530

    Update Main.py

--- Arjun/Main.py
+++ Arjun/Main.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, idle, filters 
 import os
 
-BOT_TOKEN = ""
-API_ID =
-API_HASH = ""
+BOT_TOKEN = "5465507667:AAFggC4o13JV-J-zC8yuZf3sUZa-1y5AJgk"
+API_ID = 4738674
+API_HASH = "f2be74eaa9b1cb32498f45d04e4dbb54"
 
 Sbot = Client("Sbot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
 
