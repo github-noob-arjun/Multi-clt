412a282f6966e28a055722c1d2e9d7036b6f1176
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 16:17:39 +0530

    Update start.py

--- start.py
+++ start.py
@@ -2,12 +2,14 @@ from pyrogram import Client, idle, filters
 import os
 from new.mkn import MKN
 from sample2.mkn2 import Sbot
-
+from sample3.arjun import Arjun
 
 Sbot.start()
 MKN.start()
+Arjun.start()
 
 idle()
 
 MKN.stop()
 Sbot.stop()
+Arjun.stop()
