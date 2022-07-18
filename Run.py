588044c49bc8c9231014b7154c21284e19850dd2
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-18 23:06:33 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -3,22 +3,31 @@ import os
 import asyncio
 from start import App1, App2, App3
 
-my_apps = {
-    "App1": Client("App1"),
-    "App2": Client("App2"),
-    "App3": Client("App3"),
-    # and so on
-}
+#my_apps = {
+#    "App1": Client("App1"),
+ #   "App2": Client("App2"),
+#    "App3": Client("App3"),
+#    # and so on
+#}
 
 
-for _, app in my_apps.items():
-    # Add a MessageHandler to each Client and start it
+#for _, app in my_apps.items():
+#    # Add a MessageHandler to each Client and start it
     # app.add_handler(MessageHandler(test, Filters.command("test")))
-    app.start()
+#    app.start()
 
 
-Client.idle()
+#Client.idle()
 
-for _, app in my_apps.items():
-    app.stop()
+#for _, app in my_apps.items():
+#    app.stop()
 
+App1.start()
+App2.start()
+App3.start()
+
+idle()
+
+App1.stop()
+App2.stop()
+App3.stop()
